5b5b0deb6c85638ee7d16331379d6d8bcd5e0f53
Saga_Kadam <ksagar.sk23@gmail.com>
2020-10-22 20:03:02 +0530

    timer added

--- src/components/Home/HomeView.js
+++ src/components/Home/HomeView.js
@@ -3,23 +3,62 @@ import { View, Text, TouchableOpacity } from "react-native";
 import HomeViewStyle from "./HomeViewStyle";
 import i18n from "../../i18n/i18n";
 
-const HomeView = () => {
-  return (
-    <View style={[{flex: 1}, HomeViewStyle.homeViewContainer]}>
-      <View style={{ flex: 1 }}>
-        <Text style={HomeViewStyle.welcomeHeader}>{i18n.Home.WELCOME_HEADER}</Text>
+class HomeView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0
+    };
+  }
+
+  renderStartButton() {
+    return (
+      <TouchableOpacity
+        style={HomeViewStyle.mainActionButton}
+        onPress={() => {
+          setInterval(() => {
+            //console.log("timer");
+            this.setState({
+              time: this.state.time + 1000
+            });
+          }, 1000);
+        }}
+      >
+        <Text style={HomeViewStyle.mainActionButtonText}>
+          {i18n.Home.START}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+
+  renderRunningTimer() {
+    return (
+      <TouchableOpacity
+        style={HomeViewStyle.mainActionButton}
+        onPress={() => {
+          console.log("button pressed");
+        }}
+      >
+        <Text style={HomeViewStyle.mainActionButtonText}>{this.state.time}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  render() {
+    return (
+      <View style={[{ flex: 1 }, HomeViewStyle.homeViewContainer]}>
+        <View style={{ flex: 1 }}>
+          <Text style={HomeViewStyle.welcomeHeader}>
+            {i18n.Home.WELCOME_HEADER}
+          </Text>
+        </View>
+        <View style={{ flex: 2 }}>
+          <Text>{this.state.time}</Text>
+          {this.renderStartButton()}
+        </View>
       </View>
-      <View style={{ flex: 2 }}>
-        <TouchableOpacity 
-            style={HomeViewStyle.mainActionButton}
-            onPress={() => {
-            console.log("button pressed")
-        }}>
-            <Text style={HomeViewStyle.mainActionButtonText}>{i18n.Home.START}</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 export default HomeView;
